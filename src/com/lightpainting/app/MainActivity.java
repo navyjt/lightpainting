685bcc990be9d57e319e24b3c013941bee7eae7d
Jiangtaopub <jiangtaopub@gmail.com>
2015-07-04 21:20:23 +0800

    增加颜色选项以及速度选项

--- src/com/lightpainting/app/MainActivity.java
+++ src/com/lightpainting/app/MainActivity.java
@@ -1,10 +1,18 @@
 package com.lightpainting.app;
 
+import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
@@ -14,9 +22,15 @@ import android.widget.SeekBar.OnSeekBarChangeListener;
 public class MainActivity extends Activity {
 	
 	private EditText edittextstring;
+	private TextView selcolor;
 	private Button startShowActivity;
+	private Button selColorDialog;
 	private SeekBar seekbarsize;
+	private SeekBar seekbarspeed;
 	private String fontsize = "200";
+	private String speed = "150";
+	private String strcolor ="#FFFFFF";
+	 private ColorPickerDialog dialog;
 
 
 	@Override
@@ -28,6 +42,36 @@ public class MainActivity extends Activity {
 		startShowActivity = (Button) findViewById(R.id.start_button);
 		edittextstring = (EditText) findViewById(R.id.edit_text);
 		seekbarsize = (SeekBar) findViewById(R.id.seekBarFont);
+		seekbarspeed = (SeekBar) findViewById(R.id.seekBarspeed);
+		selColorDialog = (Button) findViewById(R.id.selcolor_button);
+		selcolor = (TextView)findViewById(R.id.selcolor);
+		
+		seekbarspeed.setOnSeekBarChangeListener(new OnSeekBarChangeListener()
+		{
+
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress,
+					boolean fromUser) {
+				speed = String.valueOf(progress);
+				// TODO 自动生成的方法存根
+				
+			}
+
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+				// TODO 自动生成的方法存根
+				Toast.makeText(MainActivity.this, "越往左速度越快！", Toast.LENGTH_SHORT).show();
+			}
+
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
+				// TODO 自动生成的方法存根
+			
+			}
+		
+		});
+		
+		
 		
 		seekbarsize.setOnSeekBarChangeListener(new OnSeekBarChangeListener()
 		{
@@ -56,6 +100,79 @@ public class MainActivity extends Activity {
 		}
 		);
 
+		/*selColorDialog.setOnClickListener(new OnClickListener()   
+		{	//使用ColorPickerDialog类，不够完美
+
+			@Override
+			public void onClick(View v) {
+				dialog = new ColorPickerDialog(MainActivity.this, selcolor.getTextColors().getDefaultColor(),getResources().getString(R.string.app_name), 
+						
+						
+                        new ColorPickerDialog.OnColorChangedListener() {   
+
+                        
+
+                    public void colorChanged(int color) {   
+
+                        selcolor.setTextColor(color);   
+
+                    }   
+
+                });   
+
+                dialog.show();
+
+				// TODO 自动生成的方法存根
+				
+			}
+			
+		});*/
+		
+		selColorDialog.setOnClickListener(new OnClickListener()
+		{
+
+			@Override
+			public void onClick(View v) {
+				
+				Log.d("Laomaizi","点击按钮");
+				LinearLayout layout = new LinearLayout(MainActivity.this);  
+				layout.setOrientation(LinearLayout.VERTICAL);  
+				  
+				final TextView colorText = new TextView(MainActivity.this);  
+				ColorPickerView colorPick = new ColorPickerView(MainActivity.this,Color.parseColor("#FFFFFF"), 0.8,colorText);  
+				  
+				LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.MATCH_PARENT);  
+				lp.gravity = Gravity.CENTER_HORIZONTAL;           
+				layout.addView(colorPick, lp);  
+				layout.addView(colorText,lp);  
+				Log.d("Laomaizi","准备打开对话框");
+				
+				AlertDialog mAlertDialog =new AlertDialog.Builder(MainActivity.this).setTitle("选择一个颜色").setView(layout)  
+				.setPositiveButton(getString(R.string.dialog_color_OK), new DialogInterface.OnClickListener() 
+				{  
+				    public void onClick(DialogInterface dialog, int id) {     
+				       Log.d("laomaizi","the color choose is "+colorText.getText());  
+				   	   strcolor = (String) colorText.getText();
+					Log.d("Laomaizi","对话框返回"+strcolor);
+					selcolor.setTextColor(Color.parseColor(strcolor));
+				 }  
+				}).setNegativeButton(getString(R.string.dialog_color_Cancel), new DialogInterface.OnClickListener() 
+				{  
+				    public void onClick(DialogInterface dialog, int id) {  
+				         dialog.cancel();  
+				    }  
+				 }
+				).create(); 
+				mAlertDialog.show();
+				Log.d("Laomaizi","对话框建立"+((String) colorText.getText()));
+				//selcolor.setTextColor(Color.parseColor((String) colorText.getText()));
+			
+				
+				
+			}
+			
+		});
+		
 		startShowActivity.setOnClickListener(new OnClickListener()
 		{
 				//按钮点击事件
@@ -65,7 +182,11 @@ public class MainActivity extends Activity {
 				String data = edittextstring.getText().toString();
 				intent.putExtra("showtext",data);
 				intent.putExtra("fontsize", fontsize);
+				intent.putExtra("speed", speed);
+				intent.putExtra("color", strcolor);
 				startActivity(intent);
+				Log.d("laomaizi", "启动第二个activity");
+				//System.out.println("启动第二个activity");
 			}
 			
 		});
@@ -79,6 +200,12 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	public boolean onTouchEvent(MotionEvent e)  {
+		  // TODO Auto-generated method stub
+		Log.d("laomaizi", "第一个activity点击触发");
+		  // Toast.makeText(this, "DOWN " + e.getAction(), Toast.LENGTH_SHORT).show(); 
+		 return true; 
+	}
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// Handle action bar item clicks here. The action bar will
--- src/com/lightpainting/app/startShowActivity.java
+++ src/com/lightpainting/app/startShowActivity.java
@@ -1,16 +1,24 @@
 package com.lightpainting.app;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.TextView;
+import android.widget.Toast;
 
-public class startShowActivity extends Activity {
+public class startShowActivity extends Activity  {
 
 	private int ifont ;
+	private int ispeed;
+	private MarqueeText showtext;
+	private Vibrator vibrator;
 	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -20,15 +28,58 @@ public class startShowActivity extends Activity {
 		Intent intent = getIntent();
 		String data = intent.getStringExtra("showtext");
 		String fontsize = intent.getStringExtra("fontsize");
+		String speed = intent.getStringExtra("speed");
+		String color = intent.getStringExtra("color");
 		ifont = Integer.parseInt(fontsize);
-		TextView showtext = (TextView) findViewById(R.id.showtext);
+		ispeed = Integer.parseInt(speed);
+		showtext = (MarqueeText) findViewById(R.id.showtext);
 		showtext.setText(data);
 		showtext.setTextSize(ifont);
-		showtext.setTextColor(Color.WHITE);
-		
-		
-	
+		showtext.setTextColor(Color.parseColor(color));
+		Log.d("laomaizi", "第二个activity启动成功");
+		start(showtext,ispeed);
 	}
+	
+	public boolean onTouchEvent(MotionEvent e)  {
+		  // TODO Auto-generated method stub
+		Log.d("laomaizi", "第二个activity点击触发");
+		((Activity) this).finish();
+		  // Toast.makeText(this, "DOWN " + e.getAction(), Toast.LENGTH_SHORT).show(); 
+		 return true; 
+	}
+	
+	
+	public void start(View v,int i) {
+		
+		Log.d("laomaizi", "第二个activity 延时启动开始");
+/*		这段代码用来使移动字符延时2秒显示
+		try {
+            Thread.currentThread();
+			Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+		*/
+		
+		Log.d("laomaizi", "第二个activity 延时启动结束");
+		
+		//开始移动字符前振动一次
+		vibrator = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);  
+	    long [] pattern = {100,400,100,400};   // 停止 开启 停止 开启   
+	    vibrator.vibrate(pattern,-1);           //重复两次上面的pattern 如果只想震动一次，index设为-1   
+		showtext.startScroll(i); 
+	} 
+	
+	public void stop(View v) { 
+		showtext.stopScroll(); 
+	} 
+	
+	public void startFor0(View v){ 
+		showtext.startFor0(); 
+	} 
 }
 
 //android:theme="@style/AppTheme" >
+//android:ellipsize="marquee"
+//android:focusable="true"
+//android:focusableInTouchMode="true"
